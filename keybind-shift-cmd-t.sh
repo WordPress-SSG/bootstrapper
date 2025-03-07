662b73735f7cd8ce31d35ee8a529d72caca89fb8
Your Name <you@example.com>
2025-03-07 16:35:27 +0100

    Add

--- keybind-shift-cmd-t.sh
+++ keybind-shift-cmd-t.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 docker pull ghcr.io/wordpress-ssg/static-webpage:main
+docker pull ghcr.io/wordpress-ssg/wrangler:main
 docker pull mariadb:10.5.12
 docker pull ghcr.io/wordpress-ssg/dynamic-webpage:main
+# npx wrangler login
+# npx wrangler pages deploy ./static/
 docker compose up --build
